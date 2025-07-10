d0d9a80aa4ec4be30239f52d9fe3ccfb3686d2a7
dhruv-moradiya2 <dhruv.unikworkdevops@gmail.com>
2025-07-10 15:38:56 +0530

    add message

--- week5/connect-with-db.py
+++ week5/connect-with-db.py
@@ -1,6 +1,7 @@
 import json
 import boto3
 
+# this is connect with dynamodb
 def lambda_handler(event, context):
 
     dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
@@ -46,6 +47,47 @@ scan_param{
 res = dyanmodb.scan(**scan_parm)
 
 add in for loop and convert this list in dict
+
+
+'''
+
+
+'''
+# this is for push data using function url so takk
+import json
+import boto3 
+
+def lambda_handler(event, context):
+    
+    print("************")
+    print(event)
+    print("************")
+
+
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
+
+    table = dynamodb.Table('user-with-url')
+
+    request_body = json.loads(event['body'])
+
+    
+
+    res = table.put_item(
+        Item={
+            "username": request_body['username'],
+            "f_name": request_body['f_name'],
+            "l_name": request_body['l_name'],
+            "age": request_body['age']
+        }
+    )
+
+
+    
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(res)
+    }
 
 
 '''
